fd1d76cd4b101e24dbc7a5e26e1d717ea3fa6540
IvarSmits <info@ivarsmits.nl>
2021-04-17 18:18:47 +0200

    Add files via upload

---
+++ Javascript/Javascript/03-gebruik-if-else-statements/script.js
@@ -0,0 +1,23 @@
+const age = 17;
+
+if (age > 17) {
+  console.log("Je mag naar binnen!");
+} else {
+  console.log("Je moet helaas buiten blijven.");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+  console.log("Is vrouw");
+} else {
+  console.log("Is geen vrouw");
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+  console.log("De bestuurder mag rijden");
+} else {
+  console.log("De bestuurder mag niet rijden");
+}
