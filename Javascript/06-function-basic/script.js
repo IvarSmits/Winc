456de437b6be18d89a6b769686161b297a9553f5
IvarSmits <info@ivarsmits.nl>
2021-04-17 20:05:17 +0200

    Add files via upload

---
+++ Javascript/06-function-basic/script.js
@@ -0,0 +1,18 @@
+function makePizza() {
+  console.log("Get groceries");
+  console.log("Prepare pizza");
+  console.log("Throw pizza in oven");
+  console.log("Get pizza out of oven");
+}
+
+function makeSushi() {
+  console.log("Get groceries");
+  console.log("Prepare suhi");
+  console.log("Eat sushi");
+}
+
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
+makeSushi();
