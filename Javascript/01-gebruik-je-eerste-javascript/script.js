42dfb96c0ebdf77633bb77fdd513e36d1ff5a293
IvarSmits <info@ivarsmits.nl>
2021-04-17 18:10:09 +0200

    Add files via upload

---
+++ Javascript/01-gebruik-je-eerste-javascript/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let name = "Ivar";
+console.log(name);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(4 * 4);
+console.log(4 / 2);
+console.log(16 % 3);
+
+console.log("leeftijd:");
+let age = 24;
+
+console.log(typeof age);
