555d5e866b9ced4b18ff08bb31493779c446b8bd
ivar_smits <info@ivarsmits.nl>
2021-04-29 14:27:38 +0200

    Opdracht 16 arrowfunctions

---
+++ Javascript/16-opdracht-arrowfunctions/script.js
@@ -0,0 +1,13 @@
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+const plusFive = (num) => num + 5;
+
+const createObject = () => ({ greeting: "hoi" });
