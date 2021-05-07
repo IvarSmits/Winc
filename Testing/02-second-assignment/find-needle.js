b10c155c068de68c5bc36d3d00fd01f39ae96364
ivar_smits <info@ivarsmits.nl>
2021-05-07 14:24:10 +0200

    second assignment tests

---
+++ Testing/02-second-assignment/find-needle.js
@@ -0,0 +1,5 @@
+const findNeedle = function (words) {
+  return words.findIndex((word) => word === "needle");
+};
+
+module.exports = findNeedle;
---
+++ Testing/02-second-assignment/get-word-lengths.js
@@ -0,0 +1,5 @@
+const getWordLengths = function (words) {
+  return words.map((word) => word.length);
+};
+
+module.exports = getWordLengths;
